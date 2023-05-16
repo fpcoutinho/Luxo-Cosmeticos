6303323e00a9b6dbdd1e31cd7ed18108f733653d
fpcoutinho <fpcoutinho5@gmail.com>
2023-05-16 11:47:12 -0300

    produto model e form

--- models/produto.js
+++ models/produto.js
@@ -3,22 +3,39 @@ const Schema = mongoose.Schema;
 
 const ProdutoSchema = new Schema(
   {
-    name: {
+    nome: {
       type: String,
       required: [true, "Por favor, insira um nome."],
       minlength: [3, "O nome deve ter no mínimo 3 caracteres."],
       maxlength: [100, "O nome deve ter no máximo 100 caracteres."],
     },
-    valor: {
+    marca: {
+      type: String,
+      required: [true, "Por favor, insira uma marca."],
+    },
+    preco: {
       type: Number,
       required: [true, "Por favor, insira um valor."],
       min: [0, "O valor deve ser maior ou igual a 0."],
+    },
+    volume: {
+      type: Number,
+      required: [true, "Por favor, insira um volume."],
+      min: [0, "O volume deve ser maior ou igual a 0."],
     },
     descricao: {
       type: String,
       required: [true, "Por favor, insira uma descrição."],
       minlength: [3, "A descrição deve ter no mínimo 3 caracteres."],
       maxlength: [100, "A descrição deve ter no máximo 100 caracteres."],
+    },
+    categoria: {
+      type: String,
+      required: [true, "Por favor, insira uma categoria."],
+    },
+    genero: {
+      type: String,
+      required: [true, "Por favor, insira um gênero."],
     },
     imagemUrl: {
       type: String,
